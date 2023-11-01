f3f2b609d01a1a7c2d581129fc0091879c163c4e
Eric Sun <yfloveyq@gmail.com>
2023-11-01 09:20:33 +0800

    add further CORS configs to allow preflight request

--- controller/restful.go
+++ controller/restful.go
@@ -19,12 +19,21 @@ type RestfulController struct {
 }
 
 func NewRestfulController() *RestfulController {
-
 	r := gin.Default()
+	// all the following codes are to
 	r.Use(func() gin.HandlerFunc {
 		return func(c *gin.Context) {
 			c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+			if c.Request.Method == "OPTIONS" {
+				c.AbortWithStatus(204)
+				return
+			}
+
+			c.Next()
 		}
 	}(),
 	)
